9ec3f21f1f9406157e6df8834f2c458de141a961
Jhon9191 <joaoantonio15161516@gmail.com>
2021-02-04 22:55:29 -0300

    Adding firebase in app

--- App.js
+++ App.js
@@ -2,20 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import firebase from './src/services/firebase.js'
+
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Firebase</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
