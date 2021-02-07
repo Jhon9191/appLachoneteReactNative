ce1dc5eca7df011fa75f0910e535f7cee9bd0fa5
Jhon9191 <joaoantonio15161516@gmail.com>
2021-02-07 08:41:09 -0300

    Fixing AsyncStorage

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,5 +1,5 @@
-import { AsyncStorage } from 'react-native'
 import React, { createContext, useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage'
 
 import firebase from '../services/firebase';
 export const AuthContext = createContext({});
