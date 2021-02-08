5cc75e6b815f4a1fa201487247b7c4e50a618b44
Jhon9191 <joaoantonio15161516@gmail.com>
2021-02-08 19:57:05 -0300

    Incrementing material-top-tab-navigator

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,17 +1,20 @@
 
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+//import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import Signin from '../screens/Signin/index';
 import Signup from '../screens/Signup/index';
 
-const AuthStack = createStackNavigator();
+//const AuthStack = createStackNavigator();
+const TopStack = createMaterialTopTabNavigator();
+
 function authRoutes(){
     return(
-        <AuthStack.Navigator>
-            <AuthStack.Screen name="Signin" component={Signin}/>
-            <AuthStack.Screen name="Signup" component={Signup}/>
-        </AuthStack.Navigator>
+        <TopStack.Navigator>
+            <TopStack.Screen name="Signin" component={Signin}/>
+            <TopStack.Screen name="Signup" component={Signup}/>
+        </TopStack.Navigator>
     );
 }    
 
