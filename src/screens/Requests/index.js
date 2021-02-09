b7fdb98fef89330d4fd2d2e1c8eaefa21afa96a6
Jhon9191 <joaoantonio15161516@gmail.com>
2021-02-08 23:16:21 -0300

    Creating a menu button

--- src/screens/Requests/index.js
+++ src/screens/Requests/index.js
@@ -2,15 +2,25 @@ import React, { useState, useContext } from 'react';
 import { View, Text, TouchableOpacity, ImageBackground, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { AuthContext } from '../../context/auth';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import styles from './styles.js';
 
 export default function Home() {
   const { deslogarUsuario } = useContext(AuthContext);
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
 
   return (
     <View style={styles.background}>
+
+
+      <TouchableOpacity style={styles.menu} onPress={()=>navigation.navigate("Info")}>
+        <Icon
+          color="#FFFFFF"
+          name="bars"
+          size={24}
+        />
+      </TouchableOpacity>
 
       {/* <Button title="Deslogar" onPress={()=>deslogarUsuario()}/> */}
     </View>
--- src/screens/Requests/styles.js
+++ src/screens/Requests/styles.js
@@ -4,6 +4,11 @@ const styles = StyleSheet.create({
     background:{
         flex: 1,
         backgroundColor: '#000444'    
+    },
+
+    menu: {
+        margin: 10,
+        alignSelf: 'flex-end'
     }
 });
 
