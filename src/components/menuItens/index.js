b639ad594fc7b9dad8076cc94ec142066a5c8352
Jhon9191 <joaoantonio15161516@gmail.com>
2021-02-10 10:38:44 -0300

    Creating a modal for additions

--- src/components/menuItens/index.js
+++ src/components/menuItens/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Modal, Button } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import styles from '../menuItens/styles';
 
 export default function menuItens({ data }) {
+
+    const [visible, setVisible] = useState(false);
+    const [queijo, setQueijo] = useState(false);
+
+    function openModal() {
+        setVisible(true);
+    }
+
+    function closeModal() {
+        setVisible(false);
+    }
+
+    function confirmarPedido() {
+        setVisible(false);
+    }
+
     return (
-        <View style={{ justifyContent: "center", alignItems: 'center' }}>
+        <View style={{ justifyContent: "center", alignItems: 'center', flex: 1 }}>
+
+
             <View style={styles.container}>
 
                 <View style={styles.produto}>
@@ -14,18 +32,45 @@ export default function menuItens({ data }) {
                     <Text style={styles.price}>{data.price}</Text>
                 </View>
 
-                <View style={styles.addButton}>
-                    <TouchableOpacity>
+                <TouchableOpacity
+                    onPress={openModal}
+                    style={styles.addButton}>
+                    <View >
                         <Icon
                             name="plus"
                             size={30}
                             color="#FFF"
                         />
-                    </TouchableOpacity>
-
-                </View>
-
+                    </View>
+                </TouchableOpacity>
             </View>
+
+
+            <Modal
+                transparent={true}
+                animationType="slide"
+                visible={visible}>
+                <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+                    <View style={styles.acrecimosWindow}>
+                        <View style={styles.opcoes}>
+
+                        </View>
+                        <View style={styles.botoesFuncoes}>
+
+                            <TouchableOpacity style={styles.botaoClose} onPress={closeModal}>
+                                <Text style={styles.textClose}>Cancelar</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style={styles.botaoConfirmar} onPress={confirmarPedido}>
+                                <Text style={styles.textConfirmar}>Confirmar</Text>
+                            </TouchableOpacity>
+
+                        </View>
+                    </View>
+                </View>
+            </Modal >
+
+
         </View>
     );
 }
--- src/components/menuItens/styles.js
+++ src/components/menuItens/styles.js
@@ -43,7 +43,51 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: "bold",
         color: "#E98000"
-    }
+    },
+
+    acrecimosWindow: {
+        width: "90%", 
+        height: 600,
+        borderRadius: 40,
+        backgroundColor: "#FFFFFF", 
+        justifyContent: "center", 
+        alignItems: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.50,
+        shadowRadius: 6.68,
+        elevation: 11,
+    },
+
+    opcoes:{
+        height: "90%"
+    },
+
+    botoesFuncoes:{
+        height: "10%",
+        flexDirection: "row",
+    },
+
+    botaoClose:{
+        backgroundColor: "#FF2800",
+        padding: 10,
+        borderRadius: 20,
+        margin: 10
+    },textClose:{
+        color:"#FFFFFF",
+    },
+
+    botaoConfirmar:{
+        backgroundColor: "#32CD32",
+        padding: 10,
+        borderRadius: 20,
+        margin: 10
+    },textConfirmar:{
+        color:"#FFFFFF",
+    },
 
 });
 
