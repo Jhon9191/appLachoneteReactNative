c9039376362f0af2db0b806f1e0518e715d36e08
Jhon9191 <joaoantonio15161516@gmail.com>
2021-02-11 16:47:32 -0300

    Catch order values

--- src/screens/Wallet/index.js
+++ src/screens/Wallet/index.js
@@ -45,7 +45,8 @@ export default function Wallet() {
 
             { Pedidos.map(pedidos =>{
                 vetor.push(pedidos.preco)
-            })}
+            }
+)}
 
             
             
