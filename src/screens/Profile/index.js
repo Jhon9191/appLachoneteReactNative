c5b38fbb423960bd922e308e32480469bad18ff2
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-15 17:53:40 -0300

    Applying gradient in the background

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,8 +1,8 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { AuthContext } from '../../context/auth';
-import { LinearGradient } from 'react-native-linear-gradient'
+import LinearGradient from 'react-native-linear-gradient'
 import styles from './styles.js';
 
 export default function Profile() {
@@ -11,10 +11,11 @@ export default function Profile() {
     const navigation = useNavigation();
 
     return (
-        <View style={styles.background}>
-
-
+        <LinearGradient colors={['#FEBA67', '#f87b0f', '#d63b5d']} style={styles.background}>
             <View style={styles.card}>
+
+                <Text>Perfil</Text>
+
                 <View style={styles.Profile}>
 
                     <Text>Nome: {user.nome}</Text>
@@ -27,14 +28,27 @@ export default function Profile() {
                     <TouchableOpacity style={styles.deslogarUsuario} >
                         <Text style={styles.fontButton}>ALTERAR NOME</Text>
                     </TouchableOpacity>
+
                 </View>
-            </View>
-
-
-
-
-
-        </View>
+                </View>
+            </LinearGradient>
 
     );
 }
+
+var styless = StyleSheet.create({
+    linearGradient: {
+        flex: 1,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderRadius: 5
+    },
+    buttonText: {
+        fontSize: 18,
+        fontFamily: 'Gill Sans',
+        textAlign: 'center',
+        margin: 10,
+        color: '#ffffff',
+        backgroundColor: 'transparent',
+    },
+});
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -3,18 +3,19 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
 
     background: {
+        flex: 1,
         width: '100%',
         height: '100%',
         alignItems: 'center',
         justifyContent: 'flex-start',
-        backgroundColor: "#E98000",
     },
 
     Profile:{
         padding: 25,
         width: "90%",
         borderRadius: 25,
-        translateY: 100,
+        // translateY: '50%',
+        top: "20%",
         backgroundColor: "#fff",
         elevation: 15
     },
@@ -22,9 +23,9 @@ const styles = StyleSheet.create({
     card:{
         height: "50%",
         width: "100%",
-        borderBottomLeftRadius: 45,
-        borderBottomRightRadius: 45,
-        justifyContent: 'center',
+        borderBottomLeftRadius: 100,
+        borderBottomRightRadius: 100,
+        justifyContent: 'flex-end',
         alignItems: 'center',
         backgroundColor: "#fff",
     },
@@ -33,7 +34,7 @@ const styles = StyleSheet.create({
         marginTop: 10,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: "#FEBA67",
+        backgroundColor: "#f87b0f",
         borderRadius: 35,
         padding: 10
     },fontButton:{
