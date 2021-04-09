51424081e7d4e02381579a4c35dd8306aca21d2c
Jhon9191 <joaoantonio15161516@gmail.com>
2021-04-09 14:47:38 -0300

    Changing the style of the item in the cart

--- src/components/WalletListItem/index.js
+++ src/components/WalletListItem/index.js
@@ -14,18 +14,27 @@ export default function WalletListItem({ data }) {
     }
 
     return (
-        <View style={{ justifyContent: "center", alignItems: 'center', flex: 1,elevation: 5 }}>
+        <View style={{ justifyContent: "center", alignItems: 'center', flex: 1, elevation: 5 }}>
             <View style={styles.alinhamentoItem} key={data.key}>
                 <View style={styles.item}>
+                    <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={styles.value}>Nome: </Text>
                     <Text>{data.nome}</Text>
-                    <Text>{data.preco}</Text>
+                    </View>
+                    <Text>Acréscimos: </Text>
                     <Text>{data.acrecimos.bife == true ? "Bife: Sim" : "Bife: Não"}</Text>
                     <Text>{data.acrecimos.queijo == true ? "Queijo: Sim" : "Queijo: Não"}</Text>
                     {console.log(data)}
                 </View>
-                <TouchableOpacity style={styles.lixeira} onPress={() => handleRemove(data)}>
-                    <Icon name="trash-outline" size={30} color="#E98000" />
-                </TouchableOpacity>
+                <View style={styles.itemValue}>
+                    <Text style={styles.value}>{data.preco}</Text>
+                </View> 
+                <View style={styles.lixeira}>
+                    <TouchableOpacity onPress={() => handleRemove(data)}>
+                        <Icon name="trash-outline" size={30} color="#E98000" />
+                    </TouchableOpacity>
+
+                </View>
             </View>
         </View>
     );
--- src/components/WalletListItem/styles.js
+++ src/components/WalletListItem/styles.js
@@ -8,20 +8,32 @@ const styles = StyleSheet.create({
         width: "95%",
         padding: 15,
         backgroundColor: "#FFFFFF",
-        borderRadius: 30,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 5,
-        },
-        shadowOpacity: 2.30,
-        shadowRadius: 6.68,
-        elevation: 11,
+        //borderRadius: 5,
+        // shadowColor: "#000",
+        // shadowOffset: {
+        //     width: 0,
+        //     height: 7,
+        // },
+        // shadowOpacity: 2.30,
+        // shadowRadius: 6.68,
+        // elevation: 4,
+        borderBottomColor: "#000",
+        borderBottomWidth: 0.5,
         margin: 10
     },item:{
-        width: "85%"
+        width: "65%"    
     }, lixeira:{
+        // backgroundColor: "rgba(0,0,0,1)",
         width: '10%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }, value:{
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: "#E98000",
+        marginEnd: 5
+    },itemValue:{
+        width: '25%',
         justifyContent: 'center',
         alignItems: 'center',
     }
